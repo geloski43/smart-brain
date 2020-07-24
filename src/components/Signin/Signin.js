2887590f64147102a16e99eaa60916bf95341750
geloski43 <65926526+geloski43@users.noreply.github.com>
2020-07-24 11:53:23 +0800

    make it better

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://powerful-caverns-09293.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
